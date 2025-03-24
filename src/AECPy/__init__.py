29fec3d2346bfcacb6eca7404ec6d9fa08d10857
schaedleralmeida <schaedleralmeida@gmail.com>
2025-03-24 17:21:06 -0300

    importanto módulos no __init__

---
+++ src/AECPy/__init__.py
@@ -0,0 +1,8 @@
+from .material import Material
+from .secao import (Secao, SecaoRetangular)
+from .no import No
+from .elemento import Elemento
+
+from . import unidades
+from . import procedimentos
+from . import modelo
