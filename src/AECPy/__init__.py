8e921ca11a5bcd95d47a2961ee463f8dc566ece0
schaedleralmeida <schaedleralmeida@gmail.com>
2025-03-26 19:21:44 -0300

    importa a classe Modelo junto com o pacote

--- src/AECPy/__init__.py
+++ src/AECPy/__init__.py
@@ -2,6 +2,7 @@ from .material import Material
 from .secao import (Secao, SecaoRetangular)
 from .no import No
 from .elemento import Elemento
+from .modelo import Modelo
 
 from . import unidades
 from . import procedimentos
